32ded377eeb28f0cf5cb66ce7f6d2a8cec7f5c8d
nazarmarchuk82 <96111970+nazarmarchuk82@users.noreply.github.com>
2022-06-07 19:42:32 +0300

    Test changes in controller

--- src/main/java/ua/nm/springcourse/HelloController.java
+++ src/main/java/ua/nm/springcourse/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/hello_world")
     public String sayHello(){
         return "hello_world";
     }
